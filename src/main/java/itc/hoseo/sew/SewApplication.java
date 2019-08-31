203422f35abe2a86def811253ed8e31054ca5c21
윤동건 <dbsehdrjs20@gmail.com>
2019-08-31 10:50:56 +0900

    fisrt

---
+++ src/main/java/itc/hoseo/sew/SewApplication.java
@@ -0,0 +1,13 @@
+package itc.hoseo.sew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itc/hoseo/sew/SewApplicationTests.java
@@ -0,0 +1,16 @@
+package itc.hoseo.sew;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
