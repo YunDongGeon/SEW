3ac3ed9ebe8d05bb256814fc93b809c327b68017
YunDongGeon <dbsehdrjs20@gmail.com>
2019-08-31 15:46:30 +0900

    pom 수정 및 템플릿 변경

---
+++ src/main/resources/static/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({ 
+    	dots: true,
+    	infinite: true,
+    	speed: 500,    	
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,		
+    });
+});
