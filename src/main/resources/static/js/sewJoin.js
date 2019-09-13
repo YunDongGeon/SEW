459649942bd3a7dc1ebcb89e4def5110ac8f9802
윤동건 <dbsehdrjs20@gmail.com>
2019-09-13 14:34:53 +0900

    Update Join Terms Page CheckBox

--- src/main/resources/static/js/sewJoin.js
+++ src/main/resources/static/js/sewJoin.js
@@ -3,12 +3,21 @@ $("#allAgree").click(function(){
         //클릭되었으면
     if($("#allAgree").prop("checked")){
         //input태그의 name이 chk인 태그들을 찾아서 checked옵션을 true로 정의
-        $("input[name=smallBox]").prop("checked",true);
+        $("input[name='smallBox']").prop("checked",true);
         //클릭이 안되있으면
     }else{
         //input태그의 name이 chk인 태그들을 찾아서 checked옵션을 false로 정의
-        $("input[name=smallBox]").prop("checked",false);
+        $("input[name='smallBox']").prop("checked",false);
     }
+})
+
+// 한개의 체크박스 선택 해제시 전체선택 체크박스도 해제
+$(".smallBox").click(function(){
+	if($("input[name='smallBox']:checked").length == 2){
+		$("input[id='allAgree']").prop("checked", true);
+	}else{
+		$("input[id='allAgree']").prop("checked", false);
+	}
 })
 
 function termsChk(form){
